12ca5f9522972543820320d7553cf13550b7c36d
rohanpaul23 <paul.rohan540@gmail.com>
2025-06-18 23:10:43 +0530

    Move logic to container of component

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./toast.css"
 import type { ToastProps } from './Toast'
 import Toast from './Toast'
 
-interface ToastContainerProps  {
-    toasts : ToastProps[],
-    onToastClose : (toastId : number) => void
-}
 
-const ToastContainer = ({toasts,onToastClose}: ToastContainerProps) => {
+const ToastContainer = () => {
+  const toasts: ToastProps[] = [
+      { toastId: 1, message: 'Hey', type: 'success', duration: 3000, position: 'top-left', animation: 'fade' },
+      { toastId: 2, message: 'Hello World!', type: 'error', duration: 5000, position: 'top-left', animation: 'fade' },
+      { toastId: 3, message: 'Hello World!', type: 'info', duration: 7000, position: 'top-left', animation: 'fade' },
+      { toastId: 4, message: 'Hello World!', type: 'success', duration: 8000, position: 'top-left', animation: 'fade' },
+      { toastId: 5, message: 'Hello World!', type: 'info', duration: 9000, position: 'top-left', animation: 'fade' },
+    ];
+    const [allToasts, setAllToasts] = useState(toasts)
+  
+    const closeToast = (toastId: number) => {
+      setAllToasts(allToasts.filter((toast :ToastProps) => toast.toastId !== toastId))
+    }
+
+
   return (
     <div className='toast-container'>
         {toasts.map((toast,index) => (
-            <Toast key={index} {...toast} onToastClose={() => onToastClose(toast.toastId)}/>
+            <Toast key={index} {...toast} onToastClose={() => closeToast(toast.toastId)}/>
         ))}
     </div>
   )
