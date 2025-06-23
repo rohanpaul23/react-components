6f282529796f4e78f77104a5037321a6ae313245
rohanpaul23 <paul.rohan540@gmail.com>
2025-06-23 19:47:10 +0530

    Select Component

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import "./styles.css"
+import { FaCaretDown, FaCaretSquareDown } from "react-icons/fa";
+import { MdClear } from "react-icons/md";
+
+export interface SelectProps {
+    label : string,  
+    selectedValue: string,
+    onChange : (valueToUpdate : string) => void
+    options: any[],
+}
+
+const Select = ({label,selectedValue,onChange,options}: SelectProps) => {
+  const [showOptions,setShowOptions] = useState(false)
+
+  console.log("options",options)
+  console.log("selectedValue",selectedValue)
+
+
+  const handleSelection = (optionSelected : string) => {
+    setShowOptions(false)
+    onChange(optionSelected)
+  }
+
+  const handleSelectionOrClear = () => {
+    if(selectedValue === ""){
+      setShowOptions((prevShowOptions:boolean) => !prevShowOptions)
+    }
+    else {
+     onChange("")
+    }
+  }
+
+  return (
+  <>
+    {label !=="" && <div>{label}</div> }
+     <div className="input-container" onClick={()=> handleSelectionOrClear()}>
+      <input
+        type={"text"}
+        value={selectedValue}
+        placeholder={"Select..."}
+        className="input-box"
+      />
+      { selectedValue === "" ?
+      <span className="input-icon-right">
+        <FaCaretDown className={`${showOptions ? "" : "rotate180Deg"}`} />
+      </span> : <span className="input-icon-right">
+        <MdClear/>
+      </span> }
+    </div>
+    {showOptions && <div className='all-options'>{
+            options.map((option) => <div className='option' onClick={()=>handleSelection(option.name)} key={option.id}>{option.name}</div>)}</div>}
+  </>
+  )
+}
+
+export default Select
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,35 @@ import ToastContainer from './components/Toast'
 import type { ToastProps } from './components/Toast/Toast'
 import Button from './components/Button';
 import Modal from './components/Modal';
+import Select from './components/Select';
 
 const App: React.FC = () => {
   const [isOpen,setIsOpen] = useState(true)
 
+  const [selectedValue,setSelectedValue] = useState('')
+
+  const options = [
+    {
+      "id" : 1,
+      "name" : "Rohan",
+    },
+    {
+      "id" : 2,
+      "name" : "Paul",
+    },
+    {
+      "id" : 1,
+      "name" : "Sonu",
+    }
+  ]
   // Call the components here to render
+
+  const handleSelection = (val:any) => {
+    setSelectedValue(val)
+  }
   return (
     <div>
-     {isOpen && <Modal isOpen onClose={()=>setIsOpen(false)}>Test Modal</Modal>}
+      <Select label='Select' selectedValue={selectedValue} onChange={(value)=>handleSelection(value)} options={options}/>
     </div>
   );
 };
